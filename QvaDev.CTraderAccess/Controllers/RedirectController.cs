24963d0bb6f7765368173379dd16e240da35b059
Peter Bobvos <p.bobvos@gmail.com>
2017-10-11 17:54:39 +0200

    WebApi for cTrader access

---
+++ QvaDev.CTraderAccess/Controllers/RedirectController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace QvaDev.CTraderAccess.Controllers
+{
+    public class RedirectController : ApiController
+    {
+        public IHttpActionResult Get([FromUri]string id, string code)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ QvaDev.CTraderAccess/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QvaDev.CTraderAccess.WebApiApplication" Language="C#" %>
