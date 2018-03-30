aaede15def9197ad38d8ec77f23145d10862921a
Bobi <p.bobvos@gmail.com>
2018-03-30 10:21:44 +0300

    Clean up

--- QvaDev.Mt4Integration/Connector.cs
+++ QvaDev.Mt4Integration/Connector.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Net;
 using System.Threading;
 using log4net;
 using QvaDev.Common;
