37080ae5f87b160d9d8452ecd9361d6f2a0674a6
Bobi <p.bobvos@gmail.com>
2018-10-03 14:24:49 +0300

    TicketService optimization

--- QvaDev.Orchestration/Services/TickerService.cs
+++ QvaDev.Orchestration/Services/TickerService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using log4net;
 using QvaDev.Common.Integration;
@@ -17,28 +18,82 @@ namespace QvaDev.Orchestration.Services
     }
 
     public class TickerService : ITickerService
-	{
-		public class CsvRow
+    {
+	    private const string ZipArchivePath = "Tickers/Tickers.zip";
+
+		public class CsvRow : IEquatable<CsvRow>
 		{
 			public string Time { get; set; }
 			public decimal Ask { get; set; }
 			public decimal Bid { get; set; }
+
+			public bool Equals(CsvRow other)
+			{
+				if (ReferenceEquals(null, other)) return false;
+				if (ReferenceEquals(this, other)) return true;
+				return Ask == other.Ask && Bid == other.Bid;
+			}
+
+			public override bool Equals(object obj)
+			{
+				if (ReferenceEquals(null, obj)) return false;
+				if (ReferenceEquals(this, obj)) return true;
+				if (obj.GetType() != this.GetType()) return false;
+				return Equals((CsvRow) obj);
+			}
+
+			public override int GetHashCode()
+			{
+				unchecked
+				{
+					return (Ask.GetHashCode() * 397) ^ Bid.GetHashCode();
+				}
+			}
 		}
 
-		public class CsvRowPair
+		public class CsvRowPair : IEquatable<CsvRowPair>
 		{
 			public string Time { get; set; }
 			public decimal Ask { get; set; }
 			public decimal Bid { get; set; }
 			public decimal PairAsk { get; set; }
 			public decimal PairBid { get; set; }
+
+			public bool Equals(CsvRowPair other)
+			{
+				if (ReferenceEquals(null, other)) return false;
+				if (ReferenceEquals(this, other)) return true;
+				return Ask == other.Ask && Bid == other.Bid && PairAsk == other.PairAsk && PairBid == other.PairBid;
+			}
+
+			public override bool Equals(object obj)
+			{
+				if (ReferenceEquals(null, obj)) return false;
+				if (ReferenceEquals(this, obj)) return true;
+				if (obj.GetType() != this.GetType()) return false;
+				return Equals((CsvRowPair) obj);
+			}
+
+			public override int GetHashCode()
+			{
+				unchecked
+				{
+					var hashCode = Ask.GetHashCode();
+					hashCode = (hashCode * 397) ^ Bid.GetHashCode();
+					hashCode = (hashCode * 397) ^ PairAsk.GetHashCode();
+					hashCode = (hashCode * 397) ^ PairBid.GetHashCode();
+					return hashCode;
+				}
+			}
 		}
 
 		public class Writer
 		{
+			public bool Archived { get; set; }
 			public DateTime LastFlush { get; set; }
 			public StreamWriter StreamWriter { get; set; }
 			public CsvHelper.CsvWriter CsvWriter { get; set; }
+			public object LastRecord { get; set; }
 
 			public Writer(string file)
 			{
@@ -48,6 +103,7 @@ namespace QvaDev.Orchestration.Services
 
 			public void Disconnect()
 			{
+				StreamWriter.Flush();
 				StreamWriter.Close();
 				CsvWriter.Dispose();
 			}
@@ -67,6 +123,8 @@ namespace QvaDev.Orchestration.Services
 
         public void Start(List<Ticker> tickers)
         {
+	        using (new FileStream(ZipArchivePath, FileMode.OpenOrCreate)) { }
+
 			_tickers = tickers;
 
 			foreach (var ticker in _tickers)
@@ -93,14 +151,41 @@ namespace QvaDev.Orchestration.Services
 		public void Stop()
 		{
 			_isStarted = false;
-			foreach (var csvWriter in _csvWriters)
+			foreach (var csvWriter in _csvWriters.Where(w => !w.Value.Archived))
 				csvWriter.Value.Disconnect();
 			_csvWriters.Clear();
 		}
 
-		private void Fix_NewQuote(object sender, QuoteSet quoteSet)
+	    private void CheckArchive()
+	    {
+		    var now = DateTime.UtcNow;
+
+			var toArchive = _csvWriters
+			 .Where(w => !w.Value.Archived)
+			 .Where(w => w.Value.LastFlush.Date.AddHours(w.Value.LastFlush.Hour) != now.Date.AddHours(now.Hour))
+			 .ToList();
+
+			if (!toArchive.Any()) return;
+
+			using (var zipToOpen = new FileStream(ZipArchivePath, FileMode.OpenOrCreate))
+		    using (var zipArchive = new ZipArchive(zipToOpen, ZipArchiveMode.Update))
+			    foreach (var csvWriter in toArchive)
+			    {
+				    csvWriter.Value.Disconnect();
+				    csvWriter.Value.Archived = true;
+
+					var fileInfo = new FileInfo(csvWriter.Key);
+				    zipArchive.CreateEntryFromFile(fileInfo.FullName, fileInfo.Name);
+
+				    File.Delete(csvWriter.Key);
+			    }
+	    }
+
+	    private void Fix_NewQuote(object sender, QuoteSet quoteSet)
 		{
 			if (!_isStarted) return;
+			CheckArchive();
+
 			var connector = (IConnector)sender;
 
 			if (quoteSet?.Entries?.Any() != true) return;
@@ -117,6 +202,8 @@ namespace QvaDev.Orchestration.Services
 		private void Connector_NewTick(object sender, NewTickEventArgs e)
 		{
 			if (!_isStarted) return;
+			CheckArchive();
+
 			var connector = (IConnector)sender;
 
 			foreach (var ticker in _tickers)
@@ -155,17 +242,20 @@ namespace QvaDev.Orchestration.Services
 			symbol = Path.GetInvalidFileNameChars()
 				.Aggregate(symbol, (current, c) => current.Replace(c, '_'));
 
-			return $"Tickers/{id}_{symbol}_{DateTime.UtcNow.Date:yyyyMMdd}.csv";
+			return $"Tickers/{id}_{symbol}_{DateTime.UtcNow.Date:yyyyMMdd}_{DateTime.UtcNow:hh}.csv";
 		}
 
 		private void WriteCsv<T>(string file, T obj)
 		{
 			new FileInfo(file).Directory.Create();
-			var writer = _csvWriters.GetOrAdd(file, key => new Writer(file));
+			var writer = _csvWriters.GetOrAdd(file, key => new Writer(file) {LastFlush = DateTime.UtcNow});
 			lock (writer)
 			{
+				if (obj.Equals(writer.LastRecord)) return;
+				writer.LastRecord = obj;
 				writer.CsvWriter.WriteRecord(obj);
 				writer.CsvWriter.NextRecord();
+
 				if (DateTime.UtcNow - writer.LastFlush < new TimeSpan(0, 1, 0)) return;
 				writer.StreamWriter.Flush();
 				writer.LastFlush = DateTime.UtcNow;
@@ -175,10 +265,15 @@ namespace QvaDev.Orchestration.Services
 		private void WriteQuoteCsv(string file, QuoteSet quoteSet, int marketDepth)
 		{
 			new FileInfo(file).Directory.Create();
-			var writer = _csvWriters.GetOrAdd(file, key => new Writer(file));
+			var writer = _csvWriters.GetOrAdd(file, key => new Writer(file) { LastFlush = DateTime.UtcNow });
 			var take = marketDepth <= 0 ? quoteSet.Entries.Count : marketDepth;
+			var forLastRecord = ToLastRecord(quoteSet, take);
+
 			lock (writer)
 			{
+				if (AreQuoteSetEqual(writer.LastRecord, forLastRecord)) return;
+				writer.LastRecord = forLastRecord;
+
 				var w = writer.CsvWriter;
 				w.WriteField(DateTime.UtcNow.ToString("yyyy.MM.dd hh:mm:ss.fff"));
 				foreach (var qe in quoteSet.Entries.Take(take))
@@ -189,10 +284,32 @@ namespace QvaDev.Orchestration.Services
 					w.WriteField(qe.BidVolume);
 				}
 				w.NextRecord();
+
 				if (DateTime.UtcNow - writer.LastFlush < new TimeSpan(0, 1, 0)) return;
 				writer.StreamWriter.Flush();
 				writer.LastFlush = DateTime.UtcNow;
 			}
 		}
-	}
+
+	    private List<decimal?> ToLastRecord(QuoteSet quoteSet, int take)
+	    {
+		    var list = new List<decimal?>();
+
+		    foreach (var qe in quoteSet.Entries.Take(take))
+			    list.AddRange(new List<decimal?> { qe.Ask, qe.Bid, qe.AskVolume, qe.BidVolume });
+
+		    return list;
+	    }
+
+		private bool AreQuoteSetEqual(object lastRecord, List<decimal?> newRecord)
+	    {
+		    if (lastRecord == null) return false;
+		    if (!(lastRecord is List<decimal?> lr)) return false;
+
+		    var nr = newRecord;
+		    if (lr.Count != nr.Count) return false;
+
+		    return !lr.Where((t, i) => t != nr[i]).Any();
+	    }
+    }
 }
