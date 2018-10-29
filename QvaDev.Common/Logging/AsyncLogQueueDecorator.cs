e23266213eef597f1d46d1c2a4c838dfc0b33526
Bobi <p.bobvos@gmail.com>
2018-10-29 17:19:39 +0200

    Minor clean up

--- QvaDev.Common/Logging/AsyncLogQueueDecorator.cs
+++ QvaDev.Common/Logging/AsyncLogQueueDecorator.cs
@@ -33,11 +33,6 @@ namespace QvaDev.Common.Logging
 		private static readonly ConcurrentQueue<LogEntry> LogQueue =
 			new ConcurrentQueue<LogEntry>();
 
-		public void FatalFormat(IFormatProvider provider, string format, params object[] args)
-		{
-			throw new NotImplementedException();
-		}
-
 		public bool IsDebugEnabled => _log.IsDebugEnabled;
 		public bool IsInfoEnabled => _log.IsInfoEnabled;
 		public bool IsWarnEnabled => _log.IsWarnEnabled;
@@ -73,140 +68,50 @@ namespace QvaDev.Common.Logging
 		public void Debug(object message, Exception exception) =>
 			LogQueue.Enqueue(new LogEntry(message, exception, _log.Debug));
 
-		public void DebugFormat(string format, params object[] args)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void DebugFormat(string format, object arg0)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void DebugFormat(string format, object arg0, object arg1)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void DebugFormat(string format, object arg0, object arg1, object arg2)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void DebugFormat(IFormatProvider provider, string format, params object[] args)
-		{
-			throw new NotImplementedException();
-		}
+		public void DebugFormat(string format, params object[] args) => throw new NotImplementedException();
+		public void DebugFormat(string format, object arg0) => throw new NotImplementedException();
+		public void DebugFormat(string format, object arg0, object arg1) => throw new NotImplementedException();
+		public void DebugFormat(string format, object arg0, object arg1, object arg2) => throw new NotImplementedException();
+		public void DebugFormat(IFormatProvider provider, string format, params object[] args) => throw new NotImplementedException();
 
 		public void Info(object message) => Debug(message, null);
 		public void Info(object message, Exception exception) =>
 			LogQueue.Enqueue(new LogEntry(message, exception, _log.Info));
 
-		public void InfoFormat(string format, params object[] args)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void InfoFormat(string format, object arg0)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void InfoFormat(string format, object arg0, object arg1)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void InfoFormat(string format, object arg0, object arg1, object arg2)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void InfoFormat(IFormatProvider provider, string format, params object[] args)
-		{
-			throw new NotImplementedException();
-		}
+		public void InfoFormat(string format, params object[] args) => throw new NotImplementedException();
+		public void InfoFormat(string format, object arg0) => throw new NotImplementedException();
+		public void InfoFormat(string format, object arg0, object arg1) => throw new NotImplementedException();
+		public void InfoFormat(string format, object arg0, object arg1, object arg2) => throw new NotImplementedException();
+		public void InfoFormat(IFormatProvider provider, string format, params object[] args) => throw new NotImplementedException();
 
 		public void Warn(object message) => Warn(message, null);
 		public void Warn(object message, Exception exception) =>
 			LogQueue.Enqueue(new LogEntry(message, exception, _log.Warn));
 
-		public void WarnFormat(string format, params object[] args)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void WarnFormat(string format, object arg0)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void WarnFormat(string format, object arg0, object arg1)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void WarnFormat(string format, object arg0, object arg1, object arg2)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void WarnFormat(IFormatProvider provider, string format, params object[] args)
-		{
-			throw new NotImplementedException();
-		}
+		public void WarnFormat(string format, params object[] args) => throw new NotImplementedException();
+		public void WarnFormat(string format, object arg0) => throw new NotImplementedException();
+		public void WarnFormat(string format, object arg0, object arg1) => throw new NotImplementedException();
+		public void WarnFormat(string format, object arg0, object arg1, object arg2) => throw new NotImplementedException();
+		public void WarnFormat(IFormatProvider provider, string format, params object[] args) => throw new NotImplementedException();
 
 		public void Error(object message) => Error(message, null);
 		public void Error(object message, Exception exception) =>
 			LogQueue.Enqueue(new LogEntry(message, exception, _log.Error));
 
-		public void ErrorFormat(string format, params object[] args)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void ErrorFormat(string format, object arg0)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void ErrorFormat(string format, object arg0, object arg1)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void ErrorFormat(string format, object arg0, object arg1, object arg2)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void ErrorFormat(IFormatProvider provider, string format, params object[] args)
-		{
-			throw new NotImplementedException();
-		}
+		public void ErrorFormat(string format, params object[] args) => throw new NotImplementedException();
+		public void ErrorFormat(string format, object arg0) => throw new NotImplementedException();
+		public void ErrorFormat(string format, object arg0, object arg1) => throw new NotImplementedException();
+		public void ErrorFormat(string format, object arg0, object arg1, object arg2) => throw new NotImplementedException();
+		public void ErrorFormat(IFormatProvider provider, string format, params object[] args) => throw new NotImplementedException();
 
 		public void Fatal(object message) => Fatal(message, null);
 		public void Fatal(object message, Exception exception) =>
 			LogQueue.Enqueue(new LogEntry(message, exception, _log.Fatal));
 
-		public void FatalFormat(string format, params object[] args)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void FatalFormat(string format, object arg0)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void FatalFormat(string format, object arg0, object arg1)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void FatalFormat(string format, object arg0, object arg1, object arg2)
-		{
-			throw new NotImplementedException();
-		}
+		public void FatalFormat(string format, params object[] args) => throw new NotImplementedException();
+		public void FatalFormat(string format, object arg0) => throw new NotImplementedException();
+		public void FatalFormat(string format, object arg0, object arg1) => throw new NotImplementedException();
+		public void FatalFormat(string format, object arg0, object arg1, object arg2) => throw new NotImplementedException();
+		public void FatalFormat(IFormatProvider provider, string format, params object[] args) => throw new NotImplementedException();
 	}
 }
