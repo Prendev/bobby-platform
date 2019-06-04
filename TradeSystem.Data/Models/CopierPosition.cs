3274158f2104bbc5dda1cb572258e417c9141979
Bobi <p.bobvos@gmail.com>
2019-06-04 18:38:43 +0300

    Copier UI fix

--- TradeSystem.Data/Models/CopierPosition.cs
+++ TradeSystem.Data/Models/CopierPosition.cs
@@ -5,8 +5,8 @@ namespace TradeSystem.Data.Models
 {
 	public class CopierPosition : BaseEntity
 	{
-		public long MasterTicket { get; set; }
-		public long SlaveTicket { get; set; }
+		public long MasterTicket { get => Get<long>(); set => Set(value); }
+		public long SlaveTicket { get => Get<long>(); set => Set(value); }
 
 		[InvisibleColumn] public int CopierId { get; set; }
 		[InvisibleColumn] public Copier Copier { get; set; }
