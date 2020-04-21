6c288313d7fceb47e8b71d7f68f886f2a00a7019
Bobi <p.bobvos@gmail.com>
2020-04-21 20:09:55 +0300

    Revert "CustomUserControl"
    This reverts commit 815fea704e959cd75fc74465f0dac98e95345c26.

--- TradeSystem.Duplicat/Views/ProfilesUserControl.cs
+++ TradeSystem.Duplicat/Views/ProfilesUserControl.cs
@@ -1,11 +1,31 @@
-﻿using TradeSystem.Data.Models;
+﻿using System.Windows.Forms;
+using TradeSystem.Data.Models;
+using TradeSystem.Duplicat.ViewModel;
 
 namespace TradeSystem.Duplicat.Views
 {
-	public class ProfilesUserControl : CustomUserControl<Profile>
-	{
-		protected override string GetSelectedPropertyName() => nameof(ViewModel.SelectedProfile);
-		protected override object GetDataSource() => ViewModel.Profiles;
-		protected override void SelectItem(Profile item) => ViewModel.LoadProfileCommand(item);
-	}
+    public partial class ProfilesUserControl : UserControl, IMvvmUserControl
+    {
+        private DuplicatViewModel _viewModel;
+
+        public ProfilesUserControl()
+        {
+            InitializeComponent();
+        }
+
+        public void InitView(DuplicatViewModel viewModel)
+        {
+            _viewModel = viewModel;
+
+	        gbControl.AddBinding("Enabled", _viewModel, nameof(_viewModel.IsLoading), true);
+			properties.AddBinding("SelectedObject", _viewModel, nameof(_viewModel.SelectedProfile));
+
+			dgvProfiles.RowDoubleClick += (s, e) => _viewModel.LoadProfileCommand(dgvProfiles.GetSelectedItem<Profile>());
+		}
+
+        public void AttachDataSources()
+        {
+			dgvProfiles.DataSource = _viewModel.Profiles;
+		}
+    }
 }
