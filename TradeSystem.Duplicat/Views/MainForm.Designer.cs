44a9c01b16ad11d9c5cc3cdc1f72003d89ccbcb3
Bobi <p.bobvos@gmail.com>
2020-04-22 19:52:41 +0300

    Title

--- TradeSystem.Duplicat/Views/MainForm.Designer.cs
+++ TradeSystem.Duplicat/Views/MainForm.Designer.cs
@@ -753,7 +753,7 @@
 			this.gbControl.Size = new System.Drawing.Size(1120, 46);
 			this.gbControl.TabIndex = 1;
 			this.gbControl.TabStop = false;
-			this.gbControl.Text = "Main control panel - c25950d8 4/21/2020 8:13:31 PM";
+			this.gbControl.Text = "Main control panel - 075427b2 4/22/2020 7:31:51 PM";
 			// 
 			// label2
 			// 
