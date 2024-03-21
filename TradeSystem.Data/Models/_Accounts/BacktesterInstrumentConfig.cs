704ddd13c64dbae7e90b389b8d17a687aa68fa42
Bobi <bobi>
2024-03-21 21:35:04 +0100

    default params for backtester

--- TradeSystem.Data/Models/_Accounts/BacktesterInstrumentConfig.cs
+++ TradeSystem.Data/Models/_Accounts/BacktesterInstrumentConfig.cs
@@ -16,8 +16,8 @@ namespace TradeSystem.Data.Models
 		[DisplayName("MaxSlippage")] public int MaxSlippageInMs { get; set; }
 
 		[Required] public string Folder { get; set; }
-		public string DateTimeFormat { get; set; }
-		public string Delimeter { get; set; }
+		public string DateTimeFormat { get; set; } = "yyyy.MM.dd HH:mm:ss.fff";
+		public string Delimeter { get; set; } = ",";
 		public int DateTimeColumn { get; set; } = 0;
 		public int AskColumn { get; set; } = 1;
 		public int BidColumn { get; set; } = 2;
